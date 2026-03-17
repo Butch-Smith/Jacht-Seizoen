872d7db4013ea834a0fa995ae27d8b4afb38e366
ScootTheDuck <9021648@student.zadkine.nl>
2026-03-17 11:40:30 +0100

    Firebase linked to app small tests done

--- jacht-seizoen/src/app/page.jsx
+++ jacht-seizoen/src/app/page.jsx
@@ -1,10 +1,16 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-    <h1 className="text-blue-950">among us</h1>
-    <h2>behhhhhhhh</h2>
+    <main className="w-screen h-screen flex items-center px-6 py-10 flex-col">
+      <h1 className="text-4xl">hey whats up guys</h1>
+      <section className="flex w-full justify-center flex-col gap-4">
+        <Link href="./CreateRoomScreen">higufkfygui</Link>
+
+        <button className="w-full p-4 bg-gray-700 text-white text-center text-2xl font-bold">Maak kamer</button>
+      </section>
+    </main>
     </>
   );
 }
