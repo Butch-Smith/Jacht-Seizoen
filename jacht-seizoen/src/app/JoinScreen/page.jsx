19f0f68bfc1ab3e35bd374a513f8190799244d58
Butch-Smith <9021873@student.zadkine.nl>
2026-03-18 16:18:20 +0100

    behhhh

--- jacht-seizoen/src/app/JoinScreen/page.jsx
+++ jacht-seizoen/src/app/JoinScreen/page.jsx
@@ -11,10 +11,10 @@ const JoinScreen = () => {
             <BackButton />
             <section className="w-full h-9/10">
                 <section className="p-4 h-full flex items-center flex-col gap-5">
-                    <header className="flex items-center justify-center h-1/6">
+                    <header className="flex items-start font-bold justify-center w-full">
                         <h1 className="text-4xl text-white">Doe mee!!</h1>
                     </header>
-                    <input className="p-4 border-4 rounded-lg border-zinc-600 text-2xl font-bold bg-black text-white placeholder-zinc-600 w-full" type="text" placeholder="Kamer code..." />
+                    <input className="p-4 border-4 rounded-lg border-zinc-600 text-2xl active:border-white font-bold bg-black text-white placeholder-zinc-600 w-full" type="text" placeholder="Kamer code..." />
                     <input className="p-4 border-4 rounded-lg border-zinc-600 text-2xl font-bold bg-black text-white placeholder-zinc-600 w-full" type="text" placeholder="Jou naam..." />
                     <div className="flex w-full justify-center gap-4">
                         <button onClick={() => setPlayerRole("hunter")} className="flex border-4 border-red-500 rounded-lg bg-black text-white font-bold text-3xl flex-1 p-4 justify-center">Jager</button>
